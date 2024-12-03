0c800be58010f71c6a14334f1967320826285447
Arthur Duarte <tuiduarte@gmail.com>
2024-12-03 14:39:44 -0500

    feat: add supabase session

--- App.tsx
+++ App.tsx
@@ -6,10 +6,27 @@ import { MarketingScreen } from './src/screens/MarketingScreen';
 import { ITScreen } from './src/screens/ITScreen';
 import { FinanceQuizScreen } from './src/screens/finance/FinanceQuizScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import { useState,useEffect } from 'react';
+import { supabase } from './src/lib/supabase';
+import { Session } from '@supabase/supabase-js';
+import Auth from './src/components/Auth';
+import Account from './src/components/Account';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+    const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
     <NavigationContainer>
       <Stack.Navigator 
@@ -24,6 +41,8 @@ export default function App() {
           },
         }}
       >
+        {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+        
         <Stack.Screen 
           name="Home" 
           component={HomeScreen} 
