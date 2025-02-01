74484dd136c12b5807c6fcc1432ee7200db8f291
Arthur Duarte <tuiduarte@gmail.com>
2025-02-01 16:43:20 -0500

    feat: add rich content components

--- prolific-web/src/app/exercise/_components/rich_content/RichContent.tsx
+++ prolific-web/src/app/exercise/_components/rich_content/RichContent.tsx
@@ -1,15 +1,27 @@
-interface RichContentProps {
-  richContent: any; // You might want to type this more specifically based on your data structure
-}
+'use client';
+
+import React from 'react';
+import Table from './Table';
+import LineChart from './LineChart';
+import { RichContentProps } from './rich_content.type';
+import PieChart from './PieChart';
+import BarChart from './BarChart';
 
 export default function RichContent({ richContent }: RichContentProps) {
-  // You can expand this component to handle different types of rich content
-  // For now, we'll just render it as pre-formatted text
-  return (
-    <div className="my-4 p-4 bg-gray-50 rounded-xl">
-      <pre className="whitespace-pre-wrap text-gray-700 font-mono text-sm">
-        {JSON.stringify(richContent, null, 2)}
-      </pre>
-    </div>
-  );
+  if (!richContent) return null;
+
+  // Handle different types of rich content
+  switch (richContent.type) {
+    case 'table':
+      return <Table richContent={richContent} />;
+    case 'line':
+      return <LineChart richContent={richContent} />;
+    case 'pie':
+      return <PieChart richContent={richContent} />;
+    case 'bar':
+      return <BarChart richContent={richContent} />;
+    default:
+      console.warn('Unsupported rich content type');
+      return null;
+  }
 } 
